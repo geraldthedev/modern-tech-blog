8697fa8e780a5b1c1bb5c3c7f83ff405ca335284
Gerald <hakeem1106@gmail.com>
2022-02-08 22:24:46 -0500

    upload

---
+++ footer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ModernTechBlog
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'modern-tech-blog' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'modern-tech-blog' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'modern-tech-blog' ), 'modern-tech-blog', '<a href="http://underscores.me/">Max B</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+	
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
